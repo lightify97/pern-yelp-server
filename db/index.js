e58e2bfa7dcc885c19094ca4c36cb4016de0cc6e
Lightify97 <ramzanm461@gmail.com>
2022-02-10 22:01:27 +0500

    Ready for Deployment

--- db/index.js
+++ db/index.js
@@ -1,7 +1,12 @@
-const {Pool} = require('pg');
+const { Pool } = require("pg");
 
-const pool = new Pool();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = {
-    query: (text, params) => pool.query(text, params),
-}
+  query: (text, params) => pool.query(text, params),
+};
